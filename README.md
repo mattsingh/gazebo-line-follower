bb67ba3acf21d3653fff70efed4907874a469562
Matthew Singh <mattsingh@knights.ucf.edu>
2022-11-02 18:07:35 -0400

    Image segmentation using color masking
    Also update readme

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## Launch Gazebo Environment
 
+The `turtlebot3_world.launch` map is temporary until we find a way to make a map with yellow lines.
+
 ```
 export TURTLEBOT3_MODEL=waffle_pi
-roslaunch turtlebot3_gazebo turtlebot3_autorace.launch
+roslaunch turtlebot3_gazebo turtlebot3_world.launch
 ```
 
 > **_NOTE:_**  The 'burger' model doesn't have a camera whereas the 'waffle_pi' does. Add `export TURTLEBOT3_MODEL=waffle_pi` to .bashrc to avoid having to type this often.
--- line_follower.py
+++ line_follower.py
@@ -1,18 +1,42 @@
+import time
+import cv2 as cv
+import cv_bridge
+import numpy as np
 import rospy
 from sensor_msgs.msg import Image
-import cv2, cv_bridge
+
 
 class Follower:
 	def __init__(self):
+		self.low = np.array([0, 80, 0])
+		self.high = np.array([80, 255, 80])
+
+		self.latest_image = None
 		self.bridge = cv_bridge.CvBridge()
-		cv2.namedWindow("window", 1)
 		self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
+
+		cv.namedWindow("camera_window", cv.WINDOW_NORMAL)
+		cv.namedWindow("segment_window", cv.WINDOW_NORMAL)
 		rospy.spin()
+	
+	def __wait_for_image_initialization(self):
+		while self.latest_image is None:
+			time.sleep(.01)
+
+	def __color_mask(self, image):
+		image = image.astype(np.float32)
+		mask = cv.inRange(image, self.low, self.high)
+		result = cv.bitwise_and(image, image, mask=mask)
+		result = result.astype(np.uint8)
+		return result
+
 
 	def image_callback(self, msg):
 		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-		cv2.imshow("window", image)
-		cv2.waitKey(3)
+		self.latest_image = image
+		cv.imshow("camera_window", image)
+		cv.imshow("segment_window", self.__color_mask(image))
+		cv.waitKey(3)
 
 if __name__ == "__main__":
 	rospy.init_node('follower')
