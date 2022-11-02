d172f5760a3e69e2c05dddd033da62305f032d7d
Matthew Singh <mattsingh@knights.ucf.edu>
2022-11-02 16:31:36 -0400

    Image extraction from camera on turtlebot

---
+++ line_follower.py
@@ -0,0 +1,20 @@
+import rospy
+from sensor_msgs.msg import Image
+import cv2, cv_bridge
+
+class Follower:
+	def __init__(self):
+		self.bridge = cv_bridge.CvBridge()
+		cv2.namedWindow("window", 1)
+		self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
+		rospy.spin()
+
+	def image_callback(self, msg):
+		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+		cv2.imshow("window", image)
+		cv2.waitKey(3)
+
+if __name__ == "__main__":
+	rospy.init_node('follower')
+	follower = Follower()
+
